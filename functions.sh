e512f05e39fac9f5e6bd49b7af55009c35e7f0d0
Beatriz Bravo <beatriz.bravo-chavez@ten10.com>
2023-11-08 18:25:16 +0000

    added comments to the function to understand and to add arguments from the console

--- functions.sh
+++ functions.sh
@@ -1,8 +1,40 @@
 #!/bin/bash
 
+# Define a function to add two numbers
 add_two_num(){
+    # Create a local variable 'sum' and calculate the sum of the two arguments
     local sum=$(($1+$2))
-    echo sum of $1 and $2 is $sum
+
+    # Print the sum of the two numbers
+    echo "The sum of $1 and $2 is $sum"
 }
 
-add_two_num 2 3
+
+# Call the function 'add_two_num' with arguments 2 and 3
+#add_two_num 3 2  # en este caso vamos invocamos solo al archivo y se ejecuta con los argumentos ya iniciados
+add_two_num "$@"  # en la consola
+
+#"$@" is used to pass all arguments passed to the script to the add_two_num function from the CONSOLE
+
+#_________________________
+#To run the function. move to the folder in the console and type:bash functions.sh argumento1 argumento2
+#________________________
+
+# Special Variables:
+# $0: Name of the script being executed
+# $1, $2, $3, ...: Arguments passed to the script or function
+# $@: All arguments passed to the script or function as a list
+# $#: Number of arguments passed to the script or function
+# $?: Exit code of the last executed command
+# $$: Process ID of the current script or process
+# $!: Process ID of the last executed background command
+# $PWD: Current working directory
+# $USER: Username of the current user
+# $HOME: Home directory of the current user
+
+
+#These special variables are useful
+# for accessing relevant information
+# during the execution of a script and for performing specific tasks based on that information.
+
+
